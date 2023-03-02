f24a7da308515f0c05500987b00143e3e499be15
carabajal-facundo <facarabajal@hotmail.com>
2023-03-02 08:36:24 -0300

    se agrego todo de ultimo momento por baja de tencion en todo el dia

---
+++ js/practico1.js
@@ -0,0 +1,61 @@
+// Punto 1
+//alert("un mensaje");
+
+// Punto 2
+//document.write("Hola mundo");
+
+// Punto 3
+//document.write(3+5);
+
+// Punto 4
+/* let nombre = prompt("ingrese su nombre");
+document.write("Hola ",nombre); */
+
+// Punto 5
+/*let num1 = parseInt( prompt("ingrese un numero") );
+let num2 = parseInt( prompt("ingrese otro numero") );
+document.write(`la suma es: ${num1 + num2}`);*/
+
+// Punto 6
+/*
+let num1 = parseInt( prompt("ingrese un numero") );
+let num2 = parseInt( prompt("ingrese otro numero") );
+if(num1>num2){
+    document.write(`el mayor es ${num1}`);
+}
+else{
+    document.write(`el mayor es ${num2}`);
+}
+*/
+
+//Punto 7
+/*
+let num1 = parseInt( prompt("ingrese el primer numero") );
+let num2 = parseInt( prompt("ingrese el segundo numero") );
+let num3 = parseInt( prompt("ingrese tercer numero") );
+if(num1>num2){
+    if(num1>num3){
+        document.write(`el mayor es el primer numero`);
+    }
+    else{document.write(`el mayor es el tercer numero`);}
+}
+else if(num2>num3){
+    document.write(`el mayor es el segundo numero`);
+}
+else{
+    document.write(`el mayor es el tercer numero`);
+}
+*/
+
+// Punto 8
+/*
+let num1 = parseInt( prompt("ingrese un numero") );
+if( (num1%2)==0 ){
+    document.write(`el numero ingresado es divicible en 2`);
+}
+else{
+    document.write(`el numero ingresado no es divicible en 2`);
+}
+*/
+
+// Punto 9
